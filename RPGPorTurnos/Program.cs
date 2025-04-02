21271fc96abafcc02b737f3275ae750efbf5007f
ComSonoZZZ <gabrielramondossantosdelceu@gmail.com>
2025-04-02 12:08:31 -0300

    Update Program.cs
    Menu

--- RPGPorTurnos/Program.cs
+++ RPGPorTurnos/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+using System;
+namespace Daniel_e_Matheus___RPG_de_Turnos;
 
-namespace RPGPorTurnos
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
+        string escolha;
+        Console.WriteLine("~ MENU ~");
+        Console.WriteLine("Criar um personagem ou sair do jogo? 1- Criar, 2- Sair");
+        escolha = Console.ReadLine();
 
+        if (escolha == "1")
+        {
+            Personagem Char = new Personagem();
+            Char.criacaoPersonagem();
+
+            Itens Equips = new Itens();
+            Equips.cadastrarItem();
+
+            Mapa maps = new Mapa();
+            maps.mapa();
+
+        }
+        else if (escolha == "2")
+        {
+            Console.Clear();
+            Console.WriteLine("Pode pá então");
         }
     }
 }
